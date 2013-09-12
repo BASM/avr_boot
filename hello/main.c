595d6f1749f9f2107311d392e8053a8c982bad73
Leonid Myravjev <asm@asm.pp.ru>
2013-09-12 22:50:50 +0400

    Added HelloWorld application for BOOT loader testing

---
+++ hello/main.c
@@ -0,0 +1,54 @@
+/** 
+ * Simple HELLO WORD for boot loader testing.
+ */
+
+#include <stdio.h>
+#include <uart.h>
+#include <inttypes.h>
+#include <avr/boot.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+//////// UART /////////////////////
+
+static void putint(int number) {
+  char buff[5];
+  int i,j;
+
+  i=0;
+  while (number!=0) {
+    buff[i]=number%10;
+    number/=10;
+    i++;
+  }
+  while (i>0) putchar('0'+buff[i--]);
+}
+
+static writeall() {
+  int pages;
+  pages=getch();
+  
+  puts("Pages:");
+  putint(pages);
+
+
+  return 0;
+}
+
+int main(void) {
+
+  uart_init();
+  uart_stdio();
+
+  int i=0;
+  while (1) {
+    printf("Hello world in APPLICATION mode ;-) (%i)\n",i++);
+
+    _delay_ms(1000);
+  }
+
+
+
+  return 0;
+}
+
